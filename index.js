dd28e4c7d4c37c903ef62ff0d31031f10b9db0b4
alexandresilvacoelho-tech <alexandre.silva.coelho@escola.pr.gov.br>
2026-04-01 12:39:16 -0300

    mudo otas coisa

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ let sub = N1 - N2;
 let mult = N1 * N2;
 let div = N1 / N2;
 
-console.log = ("resultado da soma", soma)
-set (soma >= 100) = "seu numero é maiór que 100"{
- else
-set (soma <= 100)
+console.log = ("resultado da soma", (soma))
+console.log = ("resultado da sub", (sub))
+
+if (soma >= 100) {
+    console.log("seu resultado é maiór q 100")
 }
- 
+if (sub <= 0) {
+    console.log("sua subtração é maiór q 100")
+}
